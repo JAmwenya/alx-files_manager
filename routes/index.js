05cf76969ee7f97c0616701baa2e327a4c42b185
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-13 00:26:06 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
-router.get("/status", AppController.getStatus);
-router.get("/stats", AppController.getStats);
-router.post("/users", UsersController.postNew);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
