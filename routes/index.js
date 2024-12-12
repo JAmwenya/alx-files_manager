a69d4800598ec1bf10353ad7fe84aa7dae1ee887
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-12 22:24:26 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-// File path: routes/index.js
+// routes/index.js
 
 import express from "express";
 import AppController from "../controllers/AppController.js";
+import UsersController from "../controllers/UsersController.js";
 
 const router = express.Router();
 
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
+router.post("/users", UsersController.postNew);
 
 export default router;
