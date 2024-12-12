79a8da93e12c9417fbfbb01f653b0cfdd9e2ddc0
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-13 00:25:03 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 // routes/index.js
 
-import express from "express";
-import AppController from "../controllers/AppController";
-import UsersController from "../controllers/UsersController";
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
