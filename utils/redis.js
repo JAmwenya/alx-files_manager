14d33e70cf1c56c8017a2e3066791defd4e8d686
Amwenya <111055475+JAmwenya@users.noreply.github.com>
2024-12-12 23:05:54 +0300

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,48 +1,24 @@
-import redis from "redis";
+// utils/redis.js
 
-class RedisClient {
-	constructor() {
-		this.client = redis.createClient({
-			host: process.env.REDIS_HOST || "localhost",
-			port: process.env.REDIS_PORT || 6379,
-		});
+import redis from 'redis';
 
-		this.client.on("error", (err) => {
-			console.log(`Redis error: ${err}`);
-		});
-	}
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+});
 
-	isAlive() {
-		return this.client.connected;
-	}
-
-	async get(key) {
-		return new Promise((resolve, reject) => {
-			this.client.get(key, (err, data) => {
-				if (err) reject(err);
-				resolve(data);
-			});
-		});
-	}
-
-	async set(key, value, duration) {
-		return new Promise((resolve, reject) => {
-			this.client.setex(key, duration, value, (err, response) => {
-				if (err) reject(err);
-				resolve(response);
-			});
-		});
-	}
-
-	async del(key) {
-		return new Promise((resolve, reject) => {
-			this.client.del(key, (err, response) => {
-				if (err) reject(err);
-				resolve(response);
-			});
-		});
-	}
+// Check if Redis is alive by sending a ping command
+export async function isAlive() {
+  return new Promise((resolve, reject) => {
+    redisClient.ping((err, result) => {
+      if (err) {
+        console.error('Error pinging Redis:', err);
+        reject(new Error('Redis ping failed')); // Rejecting with an Error object
+      } else {
+        resolve(result === 'PONG');
+      }
+    });
+  });
 }
 
-const redisClient = new RedisClient();
 export default redisClient;
